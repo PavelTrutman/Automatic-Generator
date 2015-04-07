eb198ddf530ee185560286b7a3530ae2e0001d64
Pavel Trutman <pavel.trutman@fel.cvut.cz>
2015-04-07 23:11:12 +0200

    Implemented function "generateInputData" which generates random input data for benchmarking.

---
+++ generator/benchmark/generateInputData.m
@@ -0,0 +1,32 @@
+function [inputData] = generateInputData(~, known, ~, kngroups, maxInputs)
+  
+  % initialize variables
+  inputData = cell(maxInputs, 1);
+  
+  % sort knowns into kngroups
+  if isempty(kngroups)
+    numKnowns = ones(length(known), 1);
+  else
+    numKnowns = zeros(0, 1);
+    for i = 1:length(known)
+      if size(numKnowns, 1) < kngroups(i)
+        numKnowns(kngroups(i), 1) = 1;
+      else
+        numKnowns(kngroups(i)) = numKnowns(kngroups(i)) + 1;
+      end
+    end
+  end
+  
+  % get dimensions
+  rows = length(numKnowns);
+  cols = max(numKnowns);
+  
+  % generate sets of input data
+  for i = 1:maxInputs
+
+    % for each known generate random number
+   inputData{i, 1} = random('Normal', 0, 1, rows, cols);
+   
+  end
+  
+end
