7b3c7b531da8376a9a51999b5c2380af7baa3e8d
Pavel Trutman <pavel.trutman@fel.cvut.cz>
2017-06-07 14:47:31 +0200

    Zero coefficient instantiating bug.
    Fixing bug: When some coefficients are instanced to zero then wrong solver
    is generated.

--- generator/gbs_ParseEquations.m
+++ generator/gbs_ParseEquations.m
@@ -33,11 +33,28 @@ function [monomials, p, p_currdeg, symcoefs, maxdeg] = gbs_ParseEquations(cfg, e
         p{i}.eq = eq(i);
 
         % extract monomials & coefficents
-        [coefs, monoms] = coeffs(eq(i), unknown);
-        [coefs_rc, monoms] = coeffs(eqs(i), unknown);
-        [monoms, sortArray] = sort(monoms, 'descend');
-        p{i}.coefs = coefs(sortArray);
-        p{i}.coefs_rc = coefs_rc(sortArray);
+        [coefs_used, monoms_used] = coeffs(eq(i), unknown);
+        [coefs_rc_used, monoms_rc_used] = coeffs(eqs(i), unknown);
+
+        monoms = union(monoms_used, monoms_rc_used);
+        monoms = sort(monoms, 'descend');
+        coefs = sym(zeros(size(monoms)));
+        coefs_rc = sym(zeros(size(monoms)));
+        for j = 1:size(monoms, 2)
+          for k = 1:size(monoms_used, 2)
+            if isequal(monoms(j), monoms_used(k))
+              coefs(j) = coefs_used(k);
+            end
+          end
+          for k = 1:size(monoms_rc_used, 2)
+            if isequal(monoms(j), monoms_rc_used(k))
+              coefs_rc(j) = coefs_rc_used(k);
+            end
+          end
+        end
+
+        p{i}.coefs = coefs;
+        p{i}.coefs_rc = coefs_rc;
         p{i}.mons = arrayfun(@(x) char(x), monoms, 'UniformOutput', 0);
         p{i}.deg = MonomialsDegree(p{i}.mons, unknown);
         p{i}.maxdeg = max( sum(p{i}.deg, 2) );
