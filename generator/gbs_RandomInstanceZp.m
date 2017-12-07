ebfdb8bf0c572ab38657a76110446c7309e1ffc6
Dmitriy Korchemkin <dmitriy.korchemkin@gmail.com>
2017-12-07 20:08:30 +0300

    Implemented Zp-instancing in MATLAB-way

--- generator/gbs_RandomInstanceZp.m
+++ generator/gbs_RandomInstanceZp.m
@@ -10,46 +10,18 @@ function [eqi] = gbs_RandomInstanceZp(cfg, eq, known, unknown)
         eval(['syms ' char(mon) ';']);
     end
 
-    for mon = known
-        eval([char(mon) '=' num2str(floor(rand()*cfg.ZpGeneratorMaxNumber)) ';']);
-    end
+    random = sym(randi([1 cfg.ZpGeneratorMaxNumber - 1], 1, length(known)));
 
     fprintf('Creating random instance in Z_%d for %d equations\n', cfg.prime, length(eq));
-    
+
     reverseStr = '';
     for i=1:length(eq)
-
-        st = char(expand(eq(i)));
-        
-        % split to + and - and evaluate separately
-        ep = strfind(st, '-');
-        em = strfind(st, '+');
-        els = [ep em length(st)+1];
-        els = sort(els);
-        
-        p1 = 0;
-        prev = 1;
-        for j=1:size(els, 2)
-
-            if prev < (els(j)-1)
-                if st(prev) == '+'
-                    prev = prev+1;
-                end
-                ee = eval( st( prev:(els(j)-1)) );
-                p1 = EquationModulus(p1 + ee, unknown, cfg.prime);
-            end
-            
-            prev = els(j);
-
-            % print status
-            msg = sprintf('  instancing %d equation of %d (%2.0f %%%%)', i, length(eq), j/ size(els, 2)*100);
-            fprintf([reverseStr msg]);
-            reverseStr = repmat(sprintf('\b'), 1, length(msg) - 1);
-          
-        end
-        
-        eqi(i) = p1;
-        %eqi2(i) = eval(eq(i));
+        msg = sprintf('  instancing %d equation of %d', i, length(eq));
+        fprintf([reverseStr msg]);
+        reverseStr = repmat(sprintf('\b'), 1, length(msg));
+        [C, M] = coeffs(expand(subs(eq(i), known, random)), unknown);
+        C = mod(C, cfg.prime);
+        eqi(i) = sum(C.*M);
     end
     fprintf(reverseStr);
 end
